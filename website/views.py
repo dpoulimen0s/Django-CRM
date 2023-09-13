762e9064c8bc0b2346db3cb4193f5c488bf53e0a
dpoulimen0s <dpoulimenos.cs@gmail.com>
2023-09-13 23:33:46 +0300

    Updated the basic structure of the project

--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def terms(request):
+    return render(request, 'terms.html', {})
+
+
+def login(request):
+    return render(request, 'login.html', {})
+
+
+def signup(request):
+    return render(request, 'signup.html', {})
